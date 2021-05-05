a1b394397847dcf71f818db9b53558d2f851bdc9
Haoyu Wang <ytwanghaoyu@gmail.com>
2021-05-05 15:40:46 +0100

    implement basic log in function with fixed name and password

--- app/src/main/java/com/example/delivery1/LoginActivity.java
+++ app/src/main/java/com/example/delivery1/LoginActivity.java
@@ -1,16 +1,33 @@
 package com.example.delivery1;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private TextInputEditText eName;
+    private TextInputEditText ePassword;
+    private Button eLogin;
+    private TextView eAttemptsInfo;
+
+    private String Username = "Admin";
+    private String Password = "123456";
+    private Boolean isValid = false;
+    private int counter =5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +35,48 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        eName = findViewById(R.id.username_text);
+        ePassword = findViewById(R.id.password_text);
+        eLogin = findViewById(R.id.login_button);
+        eAttemptsInfo = findViewById(R.id.attempt_left);
+
+        eLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String inputName = eName.getText().toString();
+                String inputPassword = ePassword.getText().toString();
+                if(inputName.isEmpty()||inputPassword.isEmpty()){
+                    Toast.makeText(LoginActivity.this ,"Please enter details correctly!",Toast.LENGTH_SHORT).show();
+
+                }
+                else{
+                    isValid = validated(inputName,inputPassword);
+                    if (!isValid){
+                        counter--;
+                        Toast.makeText(LoginActivity.this ,"Incorrect! Please try again!",Toast.LENGTH_SHORT).show();
+                        eAttemptsInfo.setText("Number of attempts remaining: "+counter);
+                        if (counter==0){
+                            eLogin.setEnabled(false);
+                        }
+                    }
+                    else{
+                        Toast.makeText(LoginActivity.this ,"Login Successful!",Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                        startActivity(intent);
+                    }
+                }
+
+            }
+        });
+
     }
 
-
+    private boolean validated(String name, String password){
+        if(name.equals(Username) && password.equals(Password)){
+            return true;
+        }
+        return false;
+    }
 
 }
