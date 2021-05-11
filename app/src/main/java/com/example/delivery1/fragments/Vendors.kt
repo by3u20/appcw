ff8fff27405641268031303dae9381bfe6a98a6a
Haoyu Wang <ytwanghaoyu@gmail.com>
2021-05-11 16:25:50 +0100

    show all the same context

--- app/src/main/java/com/example/delivery1/fragments/Vendors.kt
+++ app/src/main/java/com/example/delivery1/fragments/Vendors.kt
@@ -5,7 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.delivery1.ListAdapterDelivery
 import com.example.delivery1.R
+import com.example.delivery1.data.DeliveryViewModel
 
 class Vendors : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,7 +20,19 @@ class Vendors : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_vendors, container, false)
+        val view = inflater.inflate(R.layout.fragment_vendors, container, false)
+
+        val adapter = ListAdapterDelivery()
+        var mDeliveryViewModel: DeliveryViewModel
+        val recyclerView: RecyclerView = view.findViewById(R.id.vendors_list)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        mDeliveryViewModel = ViewModelProvider(this).get(DeliveryViewModel::class.java)
+        mDeliveryViewModel.getDeliveries.observe(viewLifecycleOwner, Observer { delivery ->
+            adapter.setData(delivery)
+        })
+
+        return view
     }
+
 }
